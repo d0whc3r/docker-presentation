45d0bee2d02b74dfd29af1b2a5b7f9b47312a6e4
jpfs <jpfs@gft.com>
2018-05-23 10:28:13 +0200

    working reverse proxy inside boot2docker virtual machine and in any other linux system

--- examples/python-reverse-proxy/run.sh
+++ examples/python-reverse-proxy/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build -t friendlyhello .
 docker image ls | grep -i friendlyhello
-docker run -d -p 4000:80 friendlyhello
+docker run -d -p 4000:8000 friendlyhello
 echo "go to: http://192.168.99.100:4000/"
 curl http://192.168.99.100:4000
 echo "with reverse proxy you can now scale 'python-sample' service"
